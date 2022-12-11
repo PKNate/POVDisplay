60d88f4761edcd85d6bc9aed3eda3cdae6b4222c
Arturo <arturoraal@hotmail.es>
2022-12-10 21:35:49 -0600

    Finished Master.c

--- Master.c
+++ Master.c
@@ -15,53 +15,292 @@
 #bit CREN=0xFAB.4    // Continuous Receive Enable Bit
 #bit OERR=0xFAB.1    // Overrun Error Bit
 
+#byte PORTA = 0xF80
+#byte PORTB = 0xF81
+#byte PORTC = 0xF82
+#byte PORTD = 0xF83
+#byte PORTE = 0xF84
+#byte TRISA = 0xF92
+#byte TRISB = 0xF93
+#byte TRISC = 0xF94
+#byte TRISD = 0xF95
+#byte TRISE = 0xF96
+
+unsigned int16 constantuS=476; //
+int i;
 void fflush();
+char c;
+
+#int_rda
+void rcv_data()
+{
+   c = getchar();
+}
 
 void main()
 {
-   char s[20];
-   
-   delay_ms(1000);
+   delay_ms(100);
+   enable_interrupts(INT_RDA);
+   enable_interrupts(GLOBAL);
+   delay_ms(900);
    
    output_high(PIN_B0);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B1);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B2);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B3);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B4);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B5);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B6);
-   delay_ms(500);
+   delay_ms(1000);
    output_high(PIN_B7);
-   delay_ms(500);
-   
-   printf("Hola");
+   delay_ms(1000);
+   fflush();
+   //printf("Hola");
    
    while(true)
    {
-     if(kbhit())
+      switch (c)
       {
-         gets(s);
-         printf("Recibi un: %s\r\n",s);
+         case 'a':
+         {
+            // T
+            PORTB=0b00000001;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b00000001;
+            for(i=0;i<9;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // I
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b00000000;
+            for(i=0;i<9;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // Z
+            PORTB=0b10000001;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b11100001;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b10011001;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b10000111;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b10000001;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00000000;
+            delay_ms(124);
+            delay_us(400);
+            break;
+         }
+         
+         case 'b':
+         {
+            // E      
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b10001001;
+            for(i=0;i<19;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // M      
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b00000011;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00000110;
+            for(i=0;i<3;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00001100;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00000110;
+            for(i=0;i<3;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00000011;
+            for(i=0;i<4;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // I
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b00000000;
+            for(i=0;i<9;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // L
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b10000000;
+            for(i=0;i<19;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00000000;
+            delay_ms(110);
+            delay_us(120);
+            break;
+         }
+         
+         case 'c':
+         {
+            // C
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b10000001;
+            for(i=0;i<19;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // H
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b00001000;
+            for(i=0;i<18;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // E      
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b10001001;
+            for(i=0;i<19;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // L
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b10000000;
+            for(i=0;i<19;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            // L
+            PORTB=0b11111111;
+            delay_us(constantuS);
+            
+            PORTB=0b10000000;
+            for(i=0;i<19;i++)
+            delay_us(constantuS);
+            
+            // 
+            PORTB=0b00000000;
+            for(i=0;i<10;i++)
+            delay_us(constantuS);
+            
+            PORTB=0b00000000;
+            delay_ms(95);
+            delay_us(840);
+            
+            break;
+         }
+         
+         default:
+         {
+            PORTB=0b10101010;
+            break;
+         }
       }
-      
-      /*
-      output_toggle(PIN_B0);
-      output_toggle(PIN_B1);
-      output_toggle(PIN_B2);
-      output_toggle(PIN_B3);
-      output_toggle(PIN_B4);
-      output_toggle(PIN_B5);
-      output_toggle(PIN_B6);
-      output_toggle(PIN_B7);
-      delay_ms(1000);
-      
-      fflush();*/
    }
 }
 
--- Motortest.c
+++ Motortest.c
@@ -2,7 +2,9 @@
 #device adc=10
 #fuses HSPLL, PLL2, CPUDIV1, NOPROTECT, NOWDT, NOMCLR, NOLVP
 #use delay (clock=48M)
-#use rs232(rcv=pin_c7,xmit=pin_c6,baud=9600,bits=8,parity=n)
+//#use rs232(rcv=pin_c7,xmit=pin_c6,baud=9600,bits=8,parity=n)
+/*
+#include <lcd.c>
 
 #byte PORTA = 0xF80
 #byte PORTB = 0xF81
@@ -15,73 +17,89 @@
 #byte TRISD = 0xF95
 #byte TRISE = 0xF96
 
-int16 pwm=0;
+unsigned int32 pwm=0;
+unsigned int32 tmr0=0;
+float RPM=0;
 
 volatile int8 aux;      
-volatile int8 encoderM1=0;
-volatile int8 encoderM1_prev=0;
-volatile signed int16 pulsesM1=0;
+
 volatile int8 encoderM2=0;
 volatile int8 encoderM2_prev=0;
 volatile signed int16 pulsesM2=0;
-short flag1, flag2;
 
 #int_rb
 void rb_isr(){
-   encoderM1=(PORTB&0b00110000)>>4;
-   aux=encoderM1^encoderM1_prev;
-   flag1=0;
-   flag2=0;
-   
-   if(aux!=0&&aux!=0b00000011)
-   {
-      flag1=1;
-      if(((encoderM1_prev<<1)^encoderM1)&0b00000010)
-         pulsesM1--;
-      else
-         pulsesM1++;
-   }
-   encoderM1_prev=encoderM1;
-
    encoderM2=(PORTB&0b11000000)>>6;
    aux=encoderM2^encoderM2_prev;
    if(aux!=0&&aux!=0b00000011)
    {
-      flag2=1;
       if(((encoderM2_prev<<1)^encoderM2)&0b00000010)
+      {
          pulsesM2++;
+      }
       else
+      {
          pulsesM2--;
-   }
-            
-   encoderM2_prev=encoderM2;
+         
+      }
+   } 
    
-   if(flag1 && flag2)
-   set_timer0((int16)0);
+   if(pulsesM2>=940)
+   {
+      tmr0=get_timer0();
+      set_timer0((int16)0);
+      pulsesM2=0;
+   }
+   encoderM2_prev=encoderM2;
 }
+*/
 
 void main ()
 {
-   output_low(PIN_D0);
-   enable_interrupts(int_rb);
-   enable_interrupts(global);
-   
+   delay_ms(1000);
    setup_timer_2(T2_DIV_BY_1,255,1);
    setup_ccp1(CCP_PWM);
+   //delay_ms(500);
+   /*
+   //output_low(PIN_D0);
+   enable_interrupts(int_rb);
+   enable_interrupts(global);
+   lcd_init();
+   setup_timer_0(T0_INTERNAL|T0_DIV_256);
    
+   
+   
+   set_timer0(0);   
+   tmr0=get_timer0(); 
+   */
+   /*
    setup_adc(adc_clock_div_16);
    setup_adc_ports(AN0, VSS_VDD);
    set_adc_channel(0);
    delay_ms(500);
+   */
+   set_pwm1_duty((int16)920);
    
-   output_high(PIN_D0);
+   //output_high(PIN_D0);
    
    while (true)
    {
-      pwm=read_adc();
-      set_pwm1_duty(pwm);
-      printf("M1:%Ld\t\tM2:%Ld\r\n",pulsesM1, pulsesM2);
-      delay_ms(100);
+      //pwm=read_adc();
+      
+      
+      /*
+      RPM=TMR0;
+      RPM=RPM/(46875.0);
+      RPM=60.0/RPM;          
+      
+      lcd_putc('\f');      //LIMPIAR LA PANTALLA LCD
+      lcd_gotoxy(1,1); 
+      printf(lcd_putc,"RPM: %f",RPM); 
+      lcd_gotoxy(1,2);
+      printf(lcd_putc,"%Ld, %Ld, %Ld",pulsesM2,tmr0,pwm); 
+      //printf("M1:%Ld\t\tM2:%Ld\r\n",pulsesM1, pulsesM2);
+      delay_ms(200);
+      */
    }
 }
 
